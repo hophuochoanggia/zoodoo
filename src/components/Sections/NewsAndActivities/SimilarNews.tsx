ead56d6255a6f1448811a43e29fa0db345972a74
Nerdy Mike <mike.thenerdy@gmail.com>
2023-12-13 17:07:35 +0700

    fix mobile

--- src/components/Sections/NewsAndActivities/SimilarNews.tsx
+++ src/components/Sections/NewsAndActivities/SimilarNews.tsx
@@ -25,7 +25,7 @@ const SimilarNews: FC<ISimilarNewsProps> = ({ similarNews }) => {
           Bài viết tương tự
         </p>
       </div>
-      <div className="w-full grid grid-cols-2 lg:grid-cols-3 gap-8">
+      <div className="w-full grid grid-cols-1 lg:grid-cols-3 gap-8">
         {similarNews.items.map((item: any, index: number) => (
           <NewCard
             key={index}
