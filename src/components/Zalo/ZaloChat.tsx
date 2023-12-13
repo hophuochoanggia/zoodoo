d9d9b50aaccc58c7c1cbc0615facebe21129e519
Nerdy Mike <mike.thenerdy@gmail.com>
2023-12-13 15:04:04 +0700

    fix zalo chat position

--- src/components/Zalo/ZaloChat.tsx
+++ src/components/Zalo/ZaloChat.tsx
@@ -2,16 +2,14 @@ import React from "react";
 
 const ZaloChat = () => {
   return (
-    <div className="absolute bottom-0 right-0">
-      <div
-        className="zalo-chat-widget"
-        data-oaid="2133121175932663482"
-        data-welcome-message="Rất vui khi được hỗ trợ bạn!"
-        data-autopopup="0"
-        data-width=""
-        data-height=""
-      ></div>
-    </div>
+    <div
+      className="zalo-chat-widget"
+      data-oaid="2133121175932663482"
+      data-welcome-message="Rất vui khi được hỗ trợ bạn!"
+      data-autopopup="0"
+      data-width=""
+      data-height=""
+    ></div>
   );
 };
 
