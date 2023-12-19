ea689b0e825e14bf2df0979bb451ace27178b7c1
Gia Ho <gia@e9.digital>
2023-12-19 10:54:32 +0000

    Update file FacebookAnalytics.tsx

--- src/components/Analytic/FacebookAnalytics.tsx
+++ src/components/Analytic/FacebookAnalytics.tsx
@@ -3,7 +3,7 @@ import Script from "next/script";
 
 const FacebookAnalytics = () => {
   return (
-    <Script async="" src="https://s.zzcdn.me/ztr/ztracker.js?id=7142398452184547328" />
+    <Script id="facebook-analytics" async src="https://s.zzcdn.me/ztr/ztracker.js?id=7142398452184547328" />
   );
 };
 
